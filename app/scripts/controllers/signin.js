2997b32174e0ab75efba8ca10082c27351198ec1
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-08-24 04:16:28 +0700

    chore: delete dead files #1

--- app/scripts/controllers/signin.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('mwcCoreWebFrontendApp')
-  .controller('SigninCtrl', function($scope, sessionFactory) {
-    console.log('$scope', $scope);
-    console.log('sessionFactory', sessionFactory);
-  });
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -7,8 +7,4 @@ describe('Core Web Front end App', function() {
   it('should redirect index.html', function() {
     browser().navigateTo('/');
   });
-
-  it('should check ng-bind', function() {
-    expect(using('#mwc-nav-bar-items a').binding('menu.title')).toBe('Whirled');
-  });
 });
