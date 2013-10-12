d7e411d01cf4527db0f8ed8d897d1d5b20b44970
T. Budiman <tbudiman@gmail.com>
2013-10-12 07:32:56 +0700

    feat(kabam/app): add angular-ui dependencies

--- public/components/kabam/app.js
+++ public/components/kabam/app.js
@@ -1,17 +1,31 @@
-//=require jquery/jquery.js
-//=require angular/angular.js
-//=require angular-resource/angular-resource.js
-//=require angular-cookies/angular-cookies.js
-//=require angular-sanitize/angular-sanitize.js
-//=require angular-ui-router/release/angular-ui-router.js
-//=require states/states.js
-//=require auth/main.js
-//=require_self
-//=require kabam/controllers/index.js
+//= require jquery/jquery.js
+//= require jquery-ui/ui/jquery-ui.js
+//= require lodash/dist/lodash.js
+// bootstrap.js
+//= require select2/select2.js
+//= require angular/angular.js
+//= require angular-resource/angular-resource.js
+//= require angular-cookies/angular-cookies.js
+//= require angular-sanitize/angular-sanitize.js
+//= require angular-ui-router/release/angular-ui-router.js
+//= require angular-bootstrap/ui-bootstrap-tpls.js
+//= require angular-ui-utils/modules/utils.js
+//= require angular-ui-select2/src/select2.js
+//= require angular-grid/ng-grid-2.0.7.debug.js
+//= require fullcalendar/fullcalendar.js
+//= require angular-ui-calendar/src/calendar.js
+//= require restangular/dist/restangular.js
+
+//= require states/states.js
+//= require auth/main.js
+
+//= require_self
+
+//= require kabam/controllers/index.js
 
 'use strict';
 
-var dependencies = ['ui.router', 'kabam.auth', 'kabam.states'];
+var dependencies = ['ui.router', 'ui.bootstrap', 'kabam.auth', 'kabam.states'];
 if (window.moduleDependencies && Array.isArray(window.moduleDependencies)) {
   dependencies = dependencies.concat(window.moduleDependencies);
 }
