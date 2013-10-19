6fd3907d9cc0b0f5d2edf0bf9a5b26d0c32c01c5
T. Budiman <tbudiman@gmail.com>
2013-10-19 14:33:21 +0700

    fix(search): update to new search server

--- public/components/search/main.js
+++ public/components/search/main.js
@@ -1,105 +1,101 @@
-var searchModule = angular.module(
-  'kabam.search',
-  [
-    'ui.bootstrap', 'kabam.states'
-  ]);
+angular.module('kabam.search', [])
 
-searchModule.config([
-  'kabamStatesProvider',
-  function(kabamStatesProvider) {
-    kabamStatesProvider
-      .push([
-        {
-          name: 'search',
-          url: '/search?query',
-          templateUrl: '/assets/search/views/index.html',
-          controller: 'TypeaheadCtrl'
-        }
-      ]);
-  }
-]);
+  .config(['kabamStatesProvider',
+    function(kabamStatesProvider) {
+      kabamStatesProvider
+        .push([
+          {
+            name: 'search',
+            url: '/search?query',
+            templateUrl: '/assets/search/views/index.html',
+            controller: 'TypeaheadCtrl'
+          }
+        ]);
+    }
+  ])
 
-searchModule.controller(
-  'TypeaheadCtrl',
-  [ '$scope', '$http', '$log', '$state', '$stateParams',
-    function ($scope, $http, $log, $state, $stateParams) {
+  .controller(
+    'TypeaheadCtrl',
+    [ '$scope', '$http', '$log', '$timeout', '$state', '$stateParams', 'CONFIG',
+      function ($scope, $http, $log, $timeout, $state, $stateParams, CONFIG) {
+        var searchURL = CONFIG.searchURL || 'http://23.92.18.59:6660';
 
-      $scope.getTermsAsync = function(suggestion, callback) {
-        $http.jsonp("http://target.monimus.com:6660/search/autocomplete?callback=JSON_CALLBACK&q=" + suggestion)
-          .then(function(response) {
-            $scope.suggestions = response.data;
-            callback(response.data);
-          });
-      };
+        $scope.getTermsAsync = function(suggestion, callback) {
+          $http.jsonp(searchURL + '/search/autocomplete?callback=JSON_CALLBACK&q=' + suggestion)
+            .then(function(response) {
+              $scope.suggestions = response.data;
+              callback(response.data);
+            });
+        };
 
-      $scope.search = function(term, callback) {
-        $http.jsonp("http://target.monimus.com:6660/search?callback=JSON_CALLBACK&q=" + term)
-          .then(function(response) {
-            $scope.suggestions = response.data;
-            callback(response.data);
-          });
-      };
+        $scope.search = function(term, callback) {
+          $http.jsonp(searchURL + '/search?callback=JSON_CALLBACK&q=' + term)
+            .then(function(response) {
+              $scope.suggestions = response.data;
+              callback(response.data);
+            });
+        };
 
-      $scope.courses = true;
-      $scope.documents = true;
-      $scope.schools = true;
-      $scope.students = true;
+        $scope.courses = true;
+        $scope.documents = true;
+        $scope.schools = true;
+        $scope.students = true;
 
-      $scope.updater = function(selected) {
-        _.defer(function() {
-          $scope.$apply(function() {
-            $scope.selected = selected;
-            $scope.search(selected, function(results) {
-              $scope.selected = $scope.selected;
-              $scope.results = results.results;
+        $scope.updater = function(selected) {
+          $timeout(function() {
+            $scope.$apply(function() {
+              $scope.selected = selected;
+              $scope.search(selected, function(results) {
+                $scope.selected = $scope.selected;
+                $scope.results = results.results;
+              });
             });
           });
-        });
-      };
+        };
 
-      $scope.submit = function($event) {
-        if ($event.keyCode == 13) {
-          $scope.topUpdater($scope.selected);
-        }
-      };
+        $scope.submit = function($event) {
+          if ($event.keyCode == 13) {
+            $scope.topUpdater($scope.selected);
+          }
+        };
 
-      $scope.topUpdater = function(selected) {
-        _.defer(function() {
-          $scope.$apply(function() {
-            $scope.selected = selected;
-            $state.go('search', { query: selected });
+        $scope.topUpdater = function(selected) {
+          $timeout(function() {
+            $scope.$apply(function() {
+              $scope.selected = selected;
+              $state.go('search', { query: selected });
+            });
           });
-        });
-      };
+        };
 
-      if ($stateParams.query) {
-        $scope.selected = $stateParams.query;
-        $scope.search($stateParams.query, function(results) {
-          $scope.results = results.results;
-        });
-      } else {
-        $scope.selected = undefined;
-        $scope.results = {};
+        if ($stateParams.query) {
+          $scope.selected = $stateParams.query;
+          $scope.search($stateParams.query, function(results) {
+            $scope.results = results.results;
+          });
+        } else {
+          $scope.selected = undefined;
+          $scope.results = {};
+        }
+
+        var typeaheadOpts = {
+          source: $scope.getTermsAsync,
+          items: 20,
+          minLength: 2,
+          updater: $scope.updater
+        };
+
+        var topTypeaheadOpts = {
+          source: $scope.getTermsAsync,
+          items: 20,
+          minLength: 2,
+          updater: $scope.topUpdater
+        };
+
+        $('input.search-box').typeahead(typeaheadOpts);
+        $('input.top-search-box').typeahead(topTypeaheadOpts);
+
+        $scope.suggestions = [];
       }
-
-      var typeaheadOpts = {
-        source: $scope.getTermsAsync,
-        items: 20,
-        minLength: 2,
-        updater: $scope.updater
-      };
-
-      var topTypeaheadOpts = {
-        source: $scope.getTermsAsync,
-        items: 20,
-        minLength: 2,
-        updater: $scope.topUpdater
-      };
-
-      $('input.search-box').typeahead(typeaheadOpts);
-      $('input.top-search-box').typeahead(topTypeaheadOpts);
-
-      $scope.suggestions = [];
-    }
-  ]
-);
+    ]
+  );
