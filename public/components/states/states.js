d6ca45facd89c784d4eb97aece4fd887c0f8d935
Muhammad Ghazali <muhammadghazali@users.noreply.github.com>
2013-10-30 13:37:08 +0700

    chore(kabam.states): taking some notes
    This kabamStates provider is based on the Angular ui-router states though, so you'll need to understand how ui-router works.

--- public/components/states/states.js
+++ public/components/states/states.js
@@ -3,7 +3,7 @@ angular.module('kabam.states', []).provider('kabamStates',
   /**
    * @name kabamStatesProvider
    * @description Adds states to the application, allows to redefine a state: stated added later will redefine states
-   * added earlier
+   * added earlier. This provider is based on the Angular ui-router states though, so you'll need to understand how ui-router works.
    */
   function(){
     // storage for all states, states added later shadow states added earlier
