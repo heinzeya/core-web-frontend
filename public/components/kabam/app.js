8945d28eff3870566a101dc132074da823e47b6a
Alexander Petrovich <chopachom@gmail.com>
2013-11-15 15:49:45 +0400

    feat(app): split application dependencies and the app itself

--- public/components/kabam/app.js
+++ public/components/kabam/app.js
@@ -1,41 +1,4 @@
-//= require jquery/jquery.js
-//= require jquery-ui/ui/jquery-ui.js
-//= require lodash/dist/lodash.js
-//= require momentjs/moment.js
-//= require vendor/bootstrap.js
-//= require select2/select2.js
-//= require pines-notify/jquery.pnotify.js
-
-//= require angular/angular.js
-//= require angular-resource/angular-resource.js
-//= require angular-cookies/angular-cookies.js
-//= require angular-sanitize/angular-sanitize.js
-//= require angular-socket-io/socket.js
-//= require angular-ui-router/release/angular-ui-router.js
-//= require angular-bootstrap/ui-bootstrap-tpls.js
-//= require angular-ui-utils/modules/utils.js
-//= require angular-ui-select2/src/select2.js
-//= require angular-grid/ng-grid-2.0.7.debug.js
-//= require fullcalendar/fullcalendar.js
-//= require angular-ui-calendar/src/calendar.js
-//= require restangular/dist/restangular.js
-//= require angular-pines-notify/src/pnotify.js
-
-//= require states/states.js
-//= require auth/main.js
-//= require user/main.js
-//= require group/main.js
-//= require search/main.js
-//= require chat-xmpp/main.js
-//= require ./admin.js
-
-//= require_self
-
-//= require kabam/controllers/index.js
-
-'use strict';
-
-var dependencies = [
+kabam.use([
   'ui.router',
   'ui.bootstrap',
   'ui.notify',
@@ -47,13 +10,9 @@ var dependencies = [
   'kabam.search',
   'kabam.states',
   'ChatXmpp'
-];
+]);
 
-if (window.moduleDependencies && Array.isArray(window.moduleDependencies)) {
-  dependencies = dependencies.concat(window.moduleDependencies);
-}
-
-angular.module('kabam', dependencies)
+angular.module('kabam', kabam.modules)
   .config([
     '$stateProvider', '$urlRouterProvider', '$locationProvider', 'RestangularProvider', 'kabamStatesProvider',
     function ($stateProvider, $urlRouterProvider, $locationProvider, RestangularProvider, kabamStatesProvider) {
---
+++ public/components/kabam/loader.js
@@ -0,0 +1,37 @@
+//= require jquery/jquery.js
+//= require jquery-ui/ui/jquery-ui.js
+//= require lodash/dist/lodash.js
+//= require vendor/bootstrap.js
+//= require select2/select2.js
+//= require pines-notify/jquery.pnotify.js
+
+//= require angular/angular.js
+//= require angular-resource/angular-resource.js
+//= require angular-cookies/angular-cookies.js
+//= require angular-sanitize/angular-sanitize.js
+//= require angular-socket-io/socket.js
+//= require angular-ui-router/release/angular-ui-router.js
+//= require angular-bootstrap/ui-bootstrap-tpls.js
+//= require angular-ui-utils/modules/utils.js
+//= require angular-ui-select2/src/select2.js
+//= require angular-grid/ng-grid-2.0.7.debug.js
+//= require fullcalendar/fullcalendar.js
+//= require angular-ui-calendar/src/calendar.js
+//= require restangular/dist/restangular.js
+//= require angular-pines-notify/src/pnotify.js
+
+//= require states/states.js
+//= require auth/main.js
+//= require user/main.js
+//= require group/main.js
+//= require search/main.js
+//= require chat-xmpp/main.js
+//= require ./admin.js
+
+window.kabam = {
+  modules : [],
+  use: function(modules){
+    angular.isArray(modules) || (modules = [modules]);
+    this.modules = this.modules.concat(modules);
+  }
+};
